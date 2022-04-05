3325be931f6964afd8bbbe60ba65845d2e4abd5a
danyow <danyow@users.noreply.github.com>
2022-04-05 15:37:50 +0000

    deploy: b88c6b60010e0a49a6a4e67279abda274c7d9984

---
+++ en/assets/js/8e6f8400.4f070050.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[1036],{639:function(e){e.exports=JSON.parse('{"permalink":"/en/blog/tags/whiteglass","page":1,"postsPerPage":5,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
